20972ce73dbc7cbd7045fbf27a3e9b7cb998b871
mahirao5464 <mahirao5464@gmail.com>
2023-04-22 18:50:55 +0530

    put and post student

--- js/addStudent.js
+++ js/addStudent.js
@@ -1,4 +1,3 @@
-var srNo = 0;
 var students = [];
 
 function addStudent() {
@@ -10,7 +9,7 @@ function addStudent() {
   const updateButtonRef = document.getElementById("update-btn");
   updateButtonRef.addEventListener("click", updateStudent);
   // fetching data from endpoint
-  fetch("https://crudcrud.com/api/9114ffd40eb3483baec13139f7619556/Students")
+  fetch("https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students")
     .then((el) => el.json()) // converting result to json
     .then(apiResult); // calling apiresult function once response converted to json
 
@@ -19,13 +18,13 @@ function addStudent() {
     console.log(result);
     students = students.concat(result);
     students.forEach((el) => {
-      el.rollNumber = el._id;
+      //el.rollNumber = el._id;
       renderTable(el);
     });
   }
 }
 
-function updateStudent() {
+async function updateStudent() {
   const fullName = document.getElementById("editFullName").value;
   const fatherName = document.getElementById("editFatherName").value;
   const motherName = document.getElementById("editMotherName").value;
@@ -42,9 +41,28 @@ function updateStudent() {
     alertElmRef.style.display = "block";
     return;
   }
+
+  const studentId = document.getElementById("studentIndex").value;
+
+  const isUpdated = await updateStudentData(studentId, {
+    fullName,
+    fatherName,
+    motherName,
+    dob,
+  });
+
+  if (isUpdated) {
+    //const updatedRowElemRef = document.getElementById("row-" + studentId);
+    document.getElementById("fullName-" + studentId).innerHTML = fullName;
+    document.getElementById("fatherName-" + studentId).innerHTML = fatherName;
+    document.getElementById("motherName-" + studentId).innerHTML = motherName;
+    document.getElementById("dob-" + studentId).innerHTML = dob;
+  } else {
+    alert("Unable to update the student");
+  }
 }
 // save button click handler
-function saveButtonClickHandler(event) {
+async function saveButtonClickHandler(event) {
   const fullName = document.getElementById("fullName").value;
   const fatherName = document.getElementById("fatherName").value;
   const motherName = document.getElementById("motherName").value;
@@ -62,28 +80,33 @@ function saveButtonClickHandler(event) {
     return;
   }
 
-  var newStudent = {
-    rollNumber: ++srNo,
+  const newStudent = await postStudent({
     fullName: fullName,
     fatherName: fatherName,
     motherName: motherName,
     dob: dob,
-  };
+  });
+  // var newStudent = {
+  //   fullName: fullName,
+  //   fatherName: fatherName,
+  //   motherName: motherName,
+  //   dob: dob,
+  // };
   students.push(newStudent);
   renderTable(newStudent);
 }
 function renderTable(el) {
   const tableBodyElmRef = document.getElementById("studentTableBody");
 
-  let rowTemplate = `<tr>
-              <th scope="row">${el.rollNumber}</th>
-              <td>${el.fullName}</td>
-              <td>${el.fatherName}</td>
-              <td>${el.motherName}</td>
-              <td>${el.dob}</td>
+  let rowTemplate = `<tr id='row-${el._id}'>
+              <th scope="row">${el._id}</th>
+              <td id='fullName-${el._id}'>${el.fullName}</td>
+              <td id='fatherName-${el._id}'>${el.fatherName}</td>
+              <td id='motherName-${el._id}'>${el.motherName}</td>
+              <td id='dob-${el._id}'>${el.dob}</td>
               <td>
-                <button type="button" class="btn btn-primary btn-sm"  data-bs-toggle="modal" data-bs-target="#editModal"  id="editStudent${el.rollNumber}" onclick="showEditModal(this)" student-index = "${el.rollNumber}">Edit</button>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                <button type="button" class="btn btn-primary btn-sm"  data-bs-toggle="modal" data-bs-target="#editModal"  id="editStudent${el._id}" onclick="showEditModal(this)" student-index = "${el._id}">Edit</button>
+                <button type="button" class="btn btn-danger btn-sm" onclick="deleteStudent('${el._id}')">Delete</button>
               </td>
             </tr>`;
 
@@ -102,14 +125,75 @@ function isStudentFormValid(student) {
   }
   return true;
 }
+async function deleteStudent(studentId) {
+  console.log(studentId);
 
+  const isConfirmed = confirm("Are you sure you want to delete?");
+
+  if (isConfirmed) {
+    fetch(
+      "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students/" +
+        studentId,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((el) => {
+        document.getElementById("row-" + studentId).remove();
+      })
+      .catch((er) => {
+        alert("Enable to delete the student. See console for more information");
+        console.log(er);
+      });
+  }
+}
 function showEditModal(elm) {
-  let stdIndex = +elm.getAttribute("student-index");
+  let stdIndex = elm.getAttribute("student-index");
   document.getElementById("studentIndex").value = stdIndex;
-  let selectedStudent = students.find((el) => el.rollNumber == stdIndex);
+  let selectedStudent = students.find((el) => el._id == stdIndex);
   document.getElementById("editFullName").value = selectedStudent.fullName;
   document.getElementById("editFatherName").value = selectedStudent.fatherName;
   document.getElementById("editMotherName").value = selectedStudent.motherName;
   document.getElementById("editDob").value = selectedStudent.dob;
+}
+
+async function postStudent(student) {
+  const response = await fetch(
+    "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students",
+    {
+      method: "POST",
+      headers: [
+        ["Content-Type", "application/json"],
+        ["Content-Type", "text/plain"],
+      ],
+      body: JSON.stringify(student),
+    }
+  );
+
+  const addedStudent = await response.json();
+  console.log(addedStudent);
+
+  return addedStudent;
+}
+
+async function updateStudentData(studentId, student) {
+  const response = await fetch(
+    "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students/" +
+      studentId,
+
+    {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8", // Indicates the content
+      },
+      body: JSON.stringify(student),
+    }
+  );
+
+  if (response.ok) {
+    return true;
+  } else {
+    return false;
+  }
 }
 window.addEventListener("load", addStudent);
