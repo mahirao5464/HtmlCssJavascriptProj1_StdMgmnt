3e566b1e37be13cfecf7742d8d5a9df17cf7eae2
mahirao5464 <mahirao5464@gmail.com>
2023-04-24 18:19:19 +0530

    student update

--- js/addStudent.js
+++ js/addStudent.js
@@ -1,6 +1,10 @@
 var students = [];
+var editModal;
 
 function addStudent() {
+  editModal = new bootstrap.Modal(document.getElementById("editModal"), {
+    backdrop: "static",
+  });
   // Getting save btn ref in js
   const saveButtonRef = document.getElementById("save-btn");
   // adding click event handler to save button
@@ -9,7 +13,7 @@ function addStudent() {
   const updateButtonRef = document.getElementById("update-btn");
   updateButtonRef.addEventListener("click", updateStudent);
   // fetching data from endpoint
-  fetch("https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students")
+  fetch("https://crudcrud.com/api/e4e4cfae69e7472d9b86823b26555c6d/Students")
     .then((el) => el.json()) // converting result to json
     .then(apiResult); // calling apiresult function once response converted to json
 
@@ -57,6 +61,12 @@ async function updateStudent() {
     document.getElementById("fatherName-" + studentId).innerHTML = fatherName;
     document.getElementById("motherName-" + studentId).innerHTML = motherName;
     document.getElementById("dob-" + studentId).innerHTML = dob;
+    const arrIndex = students.findIndex((el) => el._id === studentId);
+    students[arrIndex].fullName = fullName;
+    students[arrIndex].fatherName = fatherName;
+    students[arrIndex].motherName = motherName;
+    students[arrIndex].dob = dob;
+    editModal.hide();
   } else {
     alert("Unable to update the student");
   }
@@ -105,7 +115,7 @@ function renderTable(el) {
               <td id='motherName-${el._id}'>${el.motherName}</td>
               <td id='dob-${el._id}'>${el.dob}</td>
               <td>
-                <button type="button" class="btn btn-primary btn-sm"  data-bs-toggle="modal" data-bs-target="#editModal"  id="editStudent${el._id}" onclick="showEditModal(this)" student-index = "${el._id}">Edit</button>
+                <button type="button" class="btn btn-primary btn-sm"  data-bs-toggle="modal" id="editStudent${el._id}" onclick="showEditModal(this)" student-index = "${el._id}">Edit</button>
                 <button type="button" class="btn btn-danger btn-sm" onclick="deleteStudent('${el._id}')">Delete</button>
               </td>
             </tr>`;
@@ -132,7 +142,7 @@ async function deleteStudent(studentId) {
 
   if (isConfirmed) {
     fetch(
-      "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students/" +
+      "https://crudcrud.com/api/e4e4cfae69e7472d9b86823b26555c6d/Students/" +
         studentId,
       {
         method: "DELETE",
@@ -155,11 +165,12 @@ function showEditModal(elm) {
   document.getElementById("editFatherName").value = selectedStudent.fatherName;
   document.getElementById("editMotherName").value = selectedStudent.motherName;
   document.getElementById("editDob").value = selectedStudent.dob;
+  editModal.show();
 }
 
 async function postStudent(student) {
   const response = await fetch(
-    "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students",
+    "https://crudcrud.com/api/e4e4cfae69e7472d9b86823b26555c6d/Students",
     {
       method: "POST",
       headers: [
@@ -178,7 +189,7 @@ async function postStudent(student) {
 
 async function updateStudentData(studentId, student) {
   const response = await fetch(
-    "https://crudcrud.com/api/1278901d462c48f2a3c6cdef451f1adc/Students/" +
+    "https://crudcrud.com/api/e4e4cfae69e7472d9b86823b26555c6d/Students/" +
       studentId,
 
     {
